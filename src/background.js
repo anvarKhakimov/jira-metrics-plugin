16cec59bdda77f271e3e85d091eddc61018e765a
Anvar Khakimov <anvar.khakimov1@gmail.com>
2024-02-28 20:41:24 +0400

    Fix missing port in URL construction

--- src/background.js
+++ src/background.js
@@ -3,9 +3,11 @@ const tabsData = {};
 chrome.runtime.onMessage.addListener((message, sender) => {
   if (message.type === 'openDataPage') {
     const jiraOriginalUrl = sender.tab.url;
-    const { hostname } = new URL(jiraOriginalUrl);
+    const urlObj = new URL(jiraOriginalUrl);
+    const { hostname } = urlObj;
+    const { port } = urlObj;
     const { rapidView } = message;
-    const pluginPageURL = `index.html?host=${hostname}&rapidView=${rapidView}`;
+    const pluginPageURL = `index.html?host=${hostname}${port ? `&port=${port}` : ''}&rapidView=${rapidView}`;
 
     chrome.tabs.create({ url: pluginPageURL }, (tab) => {
       tabsData[tab.id] = { jiraOriginalUrl };
