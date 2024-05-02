fbca8a1d3bbedbac20d5252529e2d035ea5b08ed
Anvar Khakimov <anvar.khakimov1@gmail.com>
2024-05-02 23:34:49 +0400

    feat: add button to jira sidebar

--- src/content.js
+++ src/content.js
@@ -15,7 +15,7 @@ function addButton() {
 
   if (buttonLocation) {
     const button = document.createElement('button');
-    button.innerText = 'Show Graph';
+    button.innerHTML = 'Analyze Metrics';
     button.id = 'trigger-jira-data-graph';
     button.className = 'aui-button';
     button.style.backgroundColor = '#deebff';
@@ -38,4 +38,36 @@ function addButton() {
   }
 }
 
+function addSidebarButton() {
+  if (document.querySelector('a[data-label="Analyze Metrics"]')) {
+    return;
+  }
+
+  const sidebarLocation = document.querySelector('a[data-label="Reports"]').closest('li');
+
+  if (sidebarLocation) {
+    const listElem = document.createElement('li');
+    listElem.innerHTML = `<a class="aui-nav-item" data-label="Analyze Metrics" href="#" aria-describedby="aui-tooltip">
+      <span aria-hidden="true" class="aui-icon aui-icon-small aui-iconfont-graph-bar"></span>
+      <span class="aui-nav-item-label" title="Analyze Metrics">Analyze Metrics</span></a>`;
+
+    sidebarLocation.after(listElem);
+
+    listElem.querySelector('a').addEventListener('click', () => {
+      if (chrome.runtime && typeof chrome.runtime.sendMessage === 'function') {
+        const rapidView = getRapidViewParam();
+        const currentPageURL = window.location.href;
+        chrome.runtime.sendMessage({
+          type: 'openDataPage',
+          rapidView,
+          currentPageURL,
+        });
+      } else {
+        console.error('chrome.runtime.sendMessage is not available.');
+      }
+    });
+  }
+}
+
 setInterval(addButton, 1000);
+setInterval(addSidebarButton, 1000);
