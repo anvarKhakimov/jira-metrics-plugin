05e8678e0e89d60333f19de4dba8afedc6d744bd
Anvar Khakimov <anvar.khakimov1@gmail.com>
2024-06-21 09:54:31 +0500

    add aciton to open plugin via icon

--- src/background.js
+++ src/background.js
@@ -1,5 +1,20 @@
 const tabsData = {};
 
+function getRapidViewParamFromUrl(url) {
+  const rapidViewParam = url.searchParams.get('rapidView');
+  if (rapidViewParam) {
+    return rapidViewParam;
+  }
+
+  const pathSegments = url.pathname.split('/');
+  const boardIndex = pathSegments.indexOf('boards');
+  if (boardIndex !== -1 && boardIndex < pathSegments.length - 1) {
+    return pathSegments[boardIndex + 1];
+  }
+
+  return 'default';
+}
+
 chrome.runtime.onMessage.addListener((message, sender) => {
   if (message.type === 'openDataPage') {
     const jiraOriginalUrl = sender.tab.url;
@@ -23,4 +38,63 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
     }
   }
   return true;
+});
+
+chrome.action.onClicked.addListener((tab) => {
+  const jiraUrlPatterns = [
+    /^http:\/\/.*\/secure\/RapidBoard\.jspa\?.*$/,
+    /^https:\/\/.*\/secure\/RapidBoard\.jspa\?.*$/,
+    /^https:\/\/.*\/jira\/software\/projects\/.*\/boards\/.*$/,
+    /^https:\/\/.*\/jira\/software\/c\/projects\/.*$/,
+  ];
+
+  const urlMatchesPattern = jiraUrlPatterns.some((pattern) => pattern.test(tab.url));
+
+  if (urlMatchesPattern) {
+    const url = new URL(tab.url);
+    const rapidView = getRapidViewParamFromUrl(url);
+
+    const pluginPageURL = `index.html?protocol=${url.protocol}&host=${url.hostname}${url.port ? `&port=${url.port}` : ''}&rapidView=${rapidView}`;
+
+    chrome.tabs.create({ url: pluginPageURL }, (newTab) => {
+      tabsData[newTab.id] = { jiraOriginalUrl: tab.url };
+    });
+  }
+});
+
+function updateIconState(tabId, changeInfo, tab) {
+  const jiraUrlPatterns = [
+    /^http:\/\/.*\/secure\/RapidBoard\.jspa\?.*$/,
+    /^https:\/\/.*\/secure\/RapidBoard\.jspa\?.*$/,
+    /^https:\/\/.*\/jira\/software\/projects\/.*\/boards\/.*$/,
+    /^https:\/\/.*\/jira\/software\/c\/projects\/.*$/,
+  ];
+
+  const urlMatchesPattern = jiraUrlPatterns.some((pattern) => pattern.test(tab.url));
+
+  if (urlMatchesPattern) {
+    chrome.action.enable(tabId);
+    chrome.action.setTitle({ tabId, title: 'Analyze Jira Metrics' });
+  } else {
+    chrome.action.disable(tabId);
+    chrome.action.setTitle({ tabId, title: 'Plugin is not available on this page' });
+  }
+}
+
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+  updateIconState(tabId, changeInfo, tab);
+});
+
+chrome.tabs.onActivated.addListener((activeInfo) => {
+  chrome.tabs.get(activeInfo.tabId, (tab) => {
+    updateIconState(activeInfo.tabId, {}, tab);
+  });
+});
+
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.tabs.query({}, (tabs) => {
+    tabs.forEach((tab) => {
+      updateIconState(tab.id, {}, tab);
+    });
+  });
 });
